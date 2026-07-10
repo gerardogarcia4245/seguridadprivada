251dfa07a9f52430d1418e2aa985e0793ca0962c
gerardogarcia4245 <gerardo.garcia.68992@gmail.com>
2026-07-09 19:02:05 -0600

    HOVER Y BOTONES DESHABILITADOS

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 
 function recursoDeshabilitado() {
 
-    alert("Este recurso se encuentra deshabilitado por fines didácticos.");
+    alert(
+        "Este recurso se encuentra deshabilitado por fines didácticos.\n\n" +
+        "El presente sitio forma parte de un laboratorio académico y algunas funciones han sido desactivadas intencionalmente."
+    );
 
 }
